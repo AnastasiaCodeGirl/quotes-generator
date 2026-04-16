eec741c8bdede52f7c3dc05dc8381aa078def65f
AnastasiiaShumeiko <48533538+AnastasiaCodeGirl@users.noreply.github.com>
2026-04-16 13:09:06 +0200

    Added basic JS

--- src/index.js
+++ src/index.js
@@ -1,1 +1,13 @@
+function generatePoem(event){
+    event.preventDefault();
 
+    new Typewriter("#poem", {
+        strings: ["hole life is a search for beauty", "But, when the beauty is found inside, the search ends and a beautiful journey begins."],
+        autostart: true,
+        delay: 1,
+        cursor: none
+    });
+}
+
+let poemFormElement = document.querySelector("#poem-generator-form");
+poemFormElement.addEventListener("submit", generatePoem);
