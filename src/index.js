f677ae80d4d7d3b2745e9d7935edffa8fe78cef2
AnastasiiaShumeiko <48533538+AnastasiaCodeGirl@users.noreply.github.com>
2026-04-16 21:12:12 +0200

    Optimizing User Experience

--- src/index.js
+++ src/index.js
@@ -1,4 +1,6 @@
 function displayPoem(response){
+    // console.log("poem generated");
+    
     //response.data.answer
     new Typewriter("#poem", {
         strings: response.data.answer,
@@ -14,13 +16,17 @@ function generatePoem(event){
     //build the API URL
     let instructionsInput = document.querySelector("#user-instructions");
     let apiKey = "fed24a4a3934t32fo5a63bbe36a70167";
-    let context = "Be precise and creative and base your response only on real quotes of famous people.Your mission is to display a 4 line quote in basic HTML and separate each new line with a <br/>. Make sure to follow the user instructions below. Sign the quote with its author using font-style italic and font-weight bold";
-    let prompt = "Find a quote related to the user input ${instructionsInput.value}, use only real quotes of famous people, don't write a generated bullshit.";
-    let apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
+    let context = "Be precise and base your response only on real quotes of famous people. Use only real quotes of famous people, only one quote per request, from 3 to 5 lines each, don't write a generated bullshit. Separate each line with a <br />. Make sure to follow the user instructions below. Translate it to Russian and show both versions English and Russian.  Sign the poem with the real author's name inside a <strong> element at the end of the poem and NOT at the beginning, and show it once don't repeat or translate names. Please start directly with quote without additional symbols";
+    let prompt = `Find a quote about ${instructionsInput.value}`;
+    let apiURL = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
+
+    let poemElement = document.querySelector('#poem');
+    poemElement.classList.remove("hidden");
+    poemElement.innerHTML = `<div class="generating">⌛Generating a quote about ${instructionsInput.value}</div>`;
     //Make a call to the API
-    console.log(apiUrl);
+    // console.log(apiURL);
     
-    axios.get(apiUrl).then(displayPoem);
+    axios.get(apiURL).then(displayPoem);
     //Dispay the generated poem
 
     
